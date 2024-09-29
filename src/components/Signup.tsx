fe30d94170e3e6a2d15312101bc53ab1a92a5b8a
THIRUNIJAYATHILAKE <thiruninavodya18@gmail.com>
2024-09-29 16:48:17 +0530

    complete signup withuout functions

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -15,6 +15,10 @@ export default function Signup(){
   });
 
 
+  function handleSignup(){
+
+  }
+
     return(
 
         <SafeAreaView>
@@ -71,6 +75,44 @@ export default function Signup(){
 
             </View>
 
+            <View>
+        <TouchableOpacity style={styles.loginButton} onPress={handleSignup}>
+          <Text style={styles.loginButtonText}>Signup</Text>
+        </TouchableOpacity>
+      </View>
+
+
+      <View>
+        <Text style={styles.haveacc}>
+            Already have an account? 
+            <Text>Login here</Text> 
+        </Text>
+      </View>
+
+
+      {/* "Or login with" Divider */}
+        <View style={styles.dividerContainer}>
+          <View style={styles.line} />
+          <Text style={styles.dividerText}>or login with</Text>
+          <View style={styles.line} />
+        </View>
+
+        <View style={styles.iconContainer}>
+          <Image
+          style={styles.logo}
+          source={require('../../assets/facebook logo_icon.png')}  // Path to the local image
+          resizeMode="contain"
+        />
+
+         <Image
+          style={styles.logo}
+          source={require('../../assets/google_g_icon.png')}  // Path to the local image
+          resizeMode="contain"
+        />
+
+        </View>
+
+
         </SafeAreaView>
 
 
@@ -110,6 +152,55 @@ const styles = StyleSheet.create({
     width: 240,
     paddingHorizontal: 10,
     marginTop: 20,
+  },
+
+
+  loginButton: {
+    backgroundColor: '#987D9A',  // Button color (purple)
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    marginTop: 40,
+    width:240,
+    borderRadius: 25,
+  },
+  loginButtonText: {
+    color: '#FFF',   // Button text color
+    fontSize: 18,
+    fontWeight: '600',
+    textAlign: 'center',
+  },
+
+  haveacc:{
+fontSize: 18,
+textAlign: 'center',
+  },
+
+  dividerContainer: {
+    flexDirection: 'row',    // To align the text and lines horizontally
+    alignItems: 'center',
+    marginVertical: 30,      // Spacing between the button and divider
+    width: '90%',            // Set the width of the container
+    alignSelf: 'center',
+  },
+  line: {
+    flex: 1,                 // Stretch the line
+    height: 1,
+    backgroundColor: 'black', // Line color
+  },
+  dividerText: {
+    marginHorizontal: 10,    // Space between the lines and the text
+    fontSize: 14,
+    color: 'black',
+  },
+  iconContainer: {
+    flexDirection: 'row',  // Aligns items vertically (default behavior)
+    alignItems: 'center',     // Centers the icons horizontally
+    marginTop: 10,            // Adds some space above the icons
+  },
+  logo: {
+    width: 35,   // Set width for each icon
+    height: 35,  // Set height for each icon
+     marginLeft: 10// Adds vertical space between the icons
   },
 
 })
